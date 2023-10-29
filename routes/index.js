f54e3b16697477c0d2554bd76a3b65ad3152f023
fromise <cddar577@gmail.com>
2023-10-29 11:36:21 -0700

    Specifying columns to return.

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/actors', function(req, res, next){
+router.get('/actors', function(req, res, next) {
   models.actor
-  .findAll({include: [{model: models.film}]})
-  .then(actorsFound => {
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify(actorsFound));
-  })
-})
+    .findAll({include: [{ model: models.film }]})
+    .then(actorsFound => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(actorsFound));
+    });
+});
+
+router.get('/actors', function(req, res, next) {
+  models.actor
+    .findAll({ 
+      attributes: ['actor_id', 'first_name', 'last_name'],
+      include: [{ 
+        model: models.film, 
+        attributes: ['film_id', 'title', 'description', 'rental_rate', 'rating'] 
+      }]      
+    })
+    .then(actorsFound => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(actorsFound));
+    });
+});
 
 module.exports = router;
