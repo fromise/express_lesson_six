91df5183b5d20a19906768c287bc0e5c65491f54
fromise <cddar577@gmail.com>
2023-10-29 11:00:35 -0700

    Finding actors and films.

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const associations = require('./rel/associations');
 
 let sequelize;
 if (config.use_env_variable) {
@@ -19,15 +19,10 @@ if (config.use_env_variable) {
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
@@ -37,7 +32,10 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
+associations(db);
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
 const mysql = require('mysql2');
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/actors', function(req, res, next){
+  models.actor
+  .findAll({include: [{model: models.film}]})
+  .then(actorsFound => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(actorsFound));
+  })
+})
 
 module.exports = router;
